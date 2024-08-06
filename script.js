e104cc6d999bcde5b36b0a29e88cc27639c147d3
prasadharbare <prasad1523@proton.me>
2024-08-06 08:06:57 +0000

    yes

---
+++ script.js
@@ -0,0 +1,32 @@
+// "use script";
+// alert("hello");
+
+fruit = "Apple"
+console.log(fruit);
+
+let friend="marker"
+console.log(friend.length);
+console.log(friend.toUpperCase());
+
+
+let MyFriend = "deepak";
+console.log(MyFriend);
+console.log(MyFriend.charAt(3));
+console.log(MyFriend.at(1));
+console.log(MyFriend.includes("p"));
+console.log(MyFriend.endsWith("h"));
+
+if (MyFriend.includes("d")) {
+    console.log("d hai");
+    }else {
+        console.log("d nai hai");
+        
+}
+let name = prompt("Enter your name");
+if (name includes(" ")) {
+    console.log("full name");
+    else {
+        console.log("first name");
+        
+    }    
+}
