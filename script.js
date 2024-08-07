7acafe2954da039c6426135849b19744f070d3ce
prasadharbare <prasad1523@proton.me>
2024-08-07 18:35:44 +0000

    ok

--- script.js
+++ script.js
@@ -22,10 +22,38 @@ if (MyFriend.includes("d")) {
         console.log("d nai hai");
         
 }
-let name = prompt("Enter your name");
-if (name.includes(" ")) {
-    console.log("full name hai");
-    }else {
-        console.log("first name hai");
-        
-    }    
+// let name = prompt("Enter your name");
+// if (name.includes(" ")) {
+//     console.log("full name hai");
+//     }else {
+//         console.log("first name hai");
+           
+  
+// const userInput = "Prasad Harbare"
+//     if (userInput.trim().includes(" ")){
+//         console.log("Its a Full Name");
+//         } else {
+//             console.log("Its a first name");
+//         }
+
+// const userInput = "Prasad Harbare";
+// console.log(userInput.trim());
+
+// function namaste() {
+//     console.log("kase ahat");
+// }
+// console.log(namaste());
+
+// function namaste(name) {
+//     console.log("namaste " + name + " kasa ahes");
+// }
+// namaste("Prasad");
+
+// function greeting(word, name) {
+//     if (word === undefined) word = "hello";
+//     if (name === undefined) name = "user";
+//      console.log(word, name);
+// }
+// greeting(" Deepak", "Prasad");
+// greeting("prasad", "gauri");
+// greeting();
