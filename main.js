885cd05982836b4a625cf990431141c221f05e0a
prasadharbare <prasad1523@proton.me>
2024-08-12 14:09:17 +0000

    k

---
+++ main.js
@@ -0,0 +1,56 @@
+// const cartoon = ["Doreamon", "Chota Bheem", "motu patlu"];
+// cartoon[] = "tom & jerry";
+// console.log(cartoon);
+
+
+// const x = Boolean();
+// console.log(x);
+
+// function isempty(list) {
+//     if (list.length === 0) return true;
+//         return false;
+//     }
+
+// function isEmpty(list) {
+//     return list.length === 0;
+// }
+
+// let x;
+// x = isEmpty([]);
+// console.log(x);
+
+// x = isEmpty([1, 2, 3]);
+// console.log(x);
+
+// function lengthyString(string) {
+//     if (string.length >= 5) return true;
+//     return false;
+// }
+// console.log(lengthyString("prasadprasad"));
+
+// function lengthAndA(str) {
+//     if (str.length >= 5 && str.includes("a")) return true;
+//     return false;
+//     }
+// let x;
+// x=lengthAndA("a")
+// console.log(x);
+
+// x = lengthAndA(["agh"], ["vhuag"]);
+// console.log(x);
+
+// const x = "hey siri hello";
+// console.log(x.split(","));
+
+// const y = [" hello","everyone"]
+// console.log(y.reverse(""));
+
+
+
+
+// function reverseString(sentence) {
+//     const stringArr=(sentence.split(""));
+//     return stringArr.reverse();
+// }
+//   const x=reverseString("Prasad Harbare")
+// console.log(x);
